81e61e76516a8011d8c78595859ac17ff3724881
Paul Gregory <pgregory@aqsis.org>
2009-04-22 10:37:23 +0100

    Combine various tools into a single handler.

---
+++ App.hx
@@ -0,0 +1,35 @@
+class App {
+	public static var request : mtwin.web.Request;
+
+	public static var template : mtwin.templo.Loader;
+
+	public static var context : Dynamic;
+
+	public static function main() {
+		mtwin.templo.Loader.BASE_DIR = neko.Web.getCwd() + "templates/";
+		mtwin.templo.Loader.TMP_DIR = neko.Web.getCwd() + "compiled/";
+		mtwin.templo.Loader.MACROS = "macros.mtt";
+		mtwin.templo.Loader.OPTIMIZED = false;
+		context = {};
+		template = null;
+
+		var cnx = neko.db.Sqlite.open(neko.Web.getCwd() + "beaker.db");
+		neko.db.Manager.cnx = cnx;
+		neko.db.Manager.initialize();
+
+		request = new mtwin.web.Request();
+		var handler = new Handler();
+
+		var level = if(request.getPathInfoPart(0) == "index.n") 1 else 0;
+
+		handler.execute(request, level);
+
+		if(template != null) {
+			neko.Web.setHeader("Context-Type", "text/html; charset=UTF-8");
+			neko.Lib.print(template.execute(context));
+		}
+
+		neko.db.Manager.cleanup();
+		cnx.close();
+	}
+}
--- Handler.hx
+++ Handler.hx
@@ -1,7 +1,23 @@
-class Convert
-{
-	static function main()
-	{
+class Handler extends mtwin.web.Handler<Void> {
+	public function new() {
+		super();
+		free("default", "view.mtt", doView);
+		free("upload", "upload.mtt", doUpload);
+		free("convert", doConvert);
+	}
+
+	public function doView() {
+		App.context.helpers = new Helpers();
+		App.context.configs = Config.manager.all();
+		App.context.tests = Test.manager.all();
+		App.context.tags = Tag.manager.objects("SELECT * FROM Tag ORDER BY POSITION", true);
+	}
+
+	public function doUpload() {
+		App.context.configs = Config.manager.all();
+	}
+
+	public function doConvert() {
 		var params = neko.Web.getMultipart(80000);
 		var alert : String = "";
 		if(haxe.Md5.encode(params.get("password")) == "bf6da5a41799708a33f0cd4dd167e4e2") {
@@ -9,15 +25,14 @@ class Convert
 		} else {
 			alert = "<div class=\"notification\">Failed: Invalid password</div>";
 		}
-		neko.Web.redirect("http://beaker.aqsis.org/Main.n");
+		neko.Web.redirect("http://beaker.aqsis.org/index.n");
 	}
 
-	static function processResultSet(config : String, resultset : String) {
-		var cnx = neko.db.Sqlite.open(neko.Web.getCwd() + "beaker.db");
+	override function prepareTemplate( t:String ) {
+		App.template = new mtwin.templo.Loader(t);
+	}
 
-		neko.db.Manager.cnx = cnx;
-		neko.db.Manager.initialize();
-	
+	public function processResultSet(config : String, resultset : String) {
 		var x : Xml = Xml.parse(resultset).elementsNamed("result_sets").next();
 		if(x != null) {
 			if(x.get("config") != "" && config == "")
@@ -50,9 +65,5 @@ class Convert
 				neko.Lib.print("Could not find specified config");
 			}
 		}
-
-		neko.db.Manager.cleanup();
-		cnx.close();
 	}
 }
-
